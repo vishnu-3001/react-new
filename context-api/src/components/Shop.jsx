00c3d3e89da24fe7edfbcdb52425fe6c6500f2ef
vishnu-3001 <vishnusai3001@yahoo.com>
2025-01-20 18:46:43 -0500

    context-api , context provider and its application

---
+++ context-api/src/components/Shop.jsx
@@ -0,0 +1,12 @@
+
+export default function Shop(props) {
+  return (
+    <section id="shop">
+      <h2>Elegant Clothing For Everyone</h2>
+      {props.children}
+      <ul id="products">
+        
+      </ul>
+    </section>
+  );
+}
