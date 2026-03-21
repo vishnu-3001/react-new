b7296e9c70f2804755f513844b0c9073e4725bd3
vishnu-3001 <vishnusai3001@yahoo.com>
2026-03-20 21:20:08 -0400

    completed diagnostic list component

--- assignment/src/Components/DiagnosticList/DiagnosticList.jsx
+++ assignment/src/Components/DiagnosticList/DiagnosticList.jsx
@@ -5,17 +5,29 @@ export default function DiagnosticList(){
         {"name":"Name","description":"Description","status":"Status"},
         {"name":"Hyper Tension","status":"under observation","description":"chronic blood pressure"}
         ,{"name":"Diabetes","status":"under observation","description":"chronic blood sugar"}
+        ,{"name":"Asthma","status":"under observation","description":"chronic respiratory condition"},
+        {"name":"Hyper Tension","status":"under observation","description":"chronic blood pressure"}
+        ,{"name":"Diabetes","status":"under observation","description":"chronic blood sugar"}
         ,{"name":"Asthma","status":"under observation","description":"chronic respiratory condition"}
     ]
     return(
         <div className={classes.diagnosticListContainer}>
             <div className={classes.diagnosticListContent}>
+                <div>
+                    <p className={classes.header}>Diagnostic List</p>
+                </div>
                 {list.map((item,index)=>{
                     return(
-                        <div key={index} className={classes.itemRow}>
-                            <p>{item.name}</p>
-                            <p>{item.status}</p>
-                            <p>{item.description}</p>
+                        <div key={index} className={index===0?classes.itemRowHeader:classes.itemRow}>
+                            <div className={classes.itemRowContent}>
+                                <p>{item.name}</p>
+                            </div>
+                            <div className={classes.itemRowContent}>
+                                <p>{item.status}</p>
+                            </div>
+                            <div className={classes.itemRowContent}>
+                                <p>{item.description}</p>
+                            </div>
                         </div>
                     )
                 })}
